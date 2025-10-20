d191d129640014eb3a6c3c0ba3e7e6aa969fe84e
Nahid-Mahbub <mahbub2305101632@diu.edu.bd>
2025-10-20 10:30:38 +0600

    1409. Queries on a Permutation With Key

--- processQueries.py
+++ processQueries.py
@@ -6,13 +6,13 @@ class Solution:
             for i in range(len(mRange)):
                 if(querie == mRange[i]):
                     print(i)
-                    counter.append(i)                   
-                    mRange.insert(0, mRange.pop(mRange[i]))
-                    print(mRange)
+                    counter.append(i)
+                    mRange.insert(0, mRange.pop(i))
+                    break
         return counter
 
 solution = Solution()
-queries = [3,1,2,1]
-m = 5
+queries = [4,1,2,2]
+m = 4
 result = solution.processQueries(queries, m)
 print(result)
