21355611d071c4f2f91372c69737e90cb1cc3003
Nahid-Mahbub <mahbub2305101632@diu.edu.bd>
2025-10-25 16:35:49 +0600

    347. Top K Frequent Elements

---
+++ topKFrequent.py
@@ -0,0 +1,20 @@
+class Solution:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        my_dic = {}
+        nums_set = set(nums)
+        print(nums_set)
+        for num in nums_set:
+            counter = 0
+            for i in range(len(nums)):
+                if(nums[i] == num):
+                    counter += 1
+            my_dic[num] = counter
+        sorted_my_dic = dict(sorted(my_dic.items(), key = lambda item: item[1], reverse = True))
+        print(sorted_my_dic)
+        return list(sorted_my_dic.keys())[:k]
+
+solution = Solution()
+nums = [4,1,-1,2,-1,2,3]
+k = 2
+result = solution.topKFrequent(nums, k)
+print(result)
