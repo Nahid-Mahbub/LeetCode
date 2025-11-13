0a0f7e8efc12c8763f407990e8f8273e5aa37305
Nahid-Mahbub <mahbub2305101632@diu.edu.bd>
2025-11-13 23:21:43 +0600

    Update solve of TwosumII and Solve of 15. 3Sum

--- twoSumII.py
+++ twoSumII.py
@@ -1,14 +1,26 @@
 class Solution:
     def twoSum(self, numbers: list[int], target: int) -> list[int]:
-        for i in range(len(numbers)):
-            value = target - numbers[i]
-            temp = numbers[i]
-            numbers[i] = "_"
-            if(value in numbers and numbers.index(value) != i):
-                print(i, numbers.index(value))
-                return [i+1, (numbers.index(value) + 1)]
-            numbers[i] = temp
-
+        # for i in range(len(numbers)):
+        #     value = target - numbers[i]
+        #     temp = numbers[i]
+        #     numbers[i] = "_"
+        #     if(value in numbers and numbers.index(value) != i):
+        #         print(i, numbers.index(value))
+        #         return [i+1, (numbers.index(value) + 1)]
+        #     numbers[i] = temp
+        # Solve 2
+        sorted(numbers)
+        left = 0
+        right = len(numbers) - 1
+        while left < right:
+            numSum = numbers[left] + numbers[right]
+            if(numSum == target):
+                return [left+1, right+1]
+            elif(numSum < target):
+                left += 1
+            elif(numSum > target):
+                right -= 1
+    
 solution = Solution()
 numbers = [1,2,3,4,4,9,56,90]
 target = 8
