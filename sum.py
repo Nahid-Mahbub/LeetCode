48c8a481f26f920087e4e6c278f954333438d18b
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2025-11-23 18:20:41 +0600

    2235. Add Two Integers

---
+++ sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2
+solution = Solution()
+num1 = 12
+num2 = 5
+result = solution.sum(num1, num2)
+print(result)
