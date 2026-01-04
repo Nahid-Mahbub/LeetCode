e165ceba4fcc35ce249c55fb40fa2117e9374e14
Nahid-Mahbub <mahbub2305101632@diu.edu.bd>
2026-01-04 11:55:33 +0600

    3668. Restore Finishing Order

---
+++ recoverOrder.py
@@ -0,0 +1,12 @@
+class Solution:
+    def recoverOrder(self, order: list[int], friends: list[int]) -> list[int]:
+        output = []
+        for num in order:
+            if(num in friends):
+                output.append(num)
+        return output
+solution = Solution()
+order = [3,1,2,5,4]
+friends = [1,3,4]
+result = solution.recoverOrder(order, friends)
+print(result)
