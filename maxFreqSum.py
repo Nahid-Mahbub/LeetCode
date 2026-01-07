90bd01f3674ba123dce45060eb2e4a5062779e9c
Nahid-Mahbub <mahbub2305101632@diu.edu.bd>
2026-01-07 22:01:25 +0600

    Fix some issues in maxFreqSum

--- maxFreqSum.py
+++ maxFreqSum.py
@@ -10,16 +10,15 @@ class Solution:
                 vowel_freq[ch] = vowel_freq.get(ch, 0) + 1
             else:
                 consonant_freq[ch] = consonant_freq.get(ch, 0) + 1
+                
+            print(vowel_freq, consonant_freq)
         
         max_vowel = max(vowel_freq.values()) if vowel_freq else 0
         max_consonant = max(consonant_freq.values()) if consonant_freq else 0
         
         return max_vowel + max_consonant
 
-
-
-# Test
 solution = Solution()
 s = "successes"
-result = solution.maxFrequencySum(s)
-print(result)  # Output: 6
+result = solution.maxFreqSum(s)
+print(result)
