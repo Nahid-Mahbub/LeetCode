783ee95a29e31bbbaa5b97f67e50856bb3a4843c
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-02-16 06:43:16 +0600

    9. Palindrome Number C#

---
+++ CSharp/LeetCode/IsPalindrome/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x = 121;
+        Console.WriteLine(IsPalindrome(x));
+    }
+
+    public static bool IsPalindrome(int x)
+    {
+        string original = x.ToString();
+
+        char[] chars = original.ToCharArray();
+        Array.Reverse(chars);
+
+        string reversed = new string(chars);
+
+        if (original == reversed)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+        // return original == reversed
+
+    }
+}
