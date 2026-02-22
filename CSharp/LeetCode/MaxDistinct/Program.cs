87722fa852d799c9d47aa2cadca31970b78f98ff
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-02-22 16:13:57 +0600

    3760. Maximum Substrings With Distinct Start C#

---
+++ CSharp/LeetCode/MaxDistinct/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string s = "abab";
+        Console.WriteLine(MaxDistinct(s));
+    }
+
+    public static int MaxDistinct(string s)
+    {
+        HashSet<char> set = new HashSet<char>();
+        foreach (char c in s)
+        {
+            set.Add(c);
+        }
+        return set.Count;
+    }
+}
