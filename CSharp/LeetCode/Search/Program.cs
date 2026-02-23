9b517e63be98fe9a420e44b08b691726db791ab6
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-02-23 14:03:32 +0600

    33. Search in Rotated Sorted Array C#

---
+++ CSharp/LeetCode/Search/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = [4, 5, 6, 7, 0, 1, 2];
+        int target = 0;
+        Console.WriteLine(Search(nums, target));
+    }
+
+    public static int Search(int[] nums, int target)
+    {
+        if (nums.Contains(target))
+            return nums.IndexOf(target);
+        else
+            return -1;
+    }
+}
