b053514811a8964984e3a028deb299b20b81e0b0
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-02-24 13:39:10 +0600

    217. Contains Duplicate C#

---
+++ CSharp/LeetCode/ContainsDuplicate/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = [1, 2, 3, 1];
+        Console.WriteLine(ContainsDuplicate(nums));
+    }
+
+    public static bool ContainsDuplicate(int[] nums)
+    {
+        HashSet<int> set = new HashSet<int>();
+        foreach (int x in nums)
+        {
+            set.Add(x);
+        }
+        return set.Count() != nums.Length;
+    }
+}
