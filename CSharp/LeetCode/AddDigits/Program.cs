3d3bad60fdf0a1f101794d565a7d080bda6141fb
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-02-26 16:16:34 +0600

    258. Add Digits C#

---
+++ CSharp/LeetCode/AddDigits/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num = 38;
+        Console.WriteLine(AddDigits(num));
+    }
+
+    public static int AddDigits(int num)
+    {
+        while(true)
+        {
+            string numS = num.ToString();
+            if (numS.Length < 2)
+            {
+                return int.Parse(numS.ToString());
+            }
+
+            int temp = 0;
+            foreach (char c in numS)
+            {
+                temp += c - '0';
+            }
+            num = temp;
+        }
+    }
+}
