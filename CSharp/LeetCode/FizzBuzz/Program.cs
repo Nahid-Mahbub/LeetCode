7b892ea6822953b00fa496f04a7e2c07400d192f
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-02-27 16:26:05 +0600

    412. Fizz Buzz C#

---
+++ CSharp/LeetCode/FizzBuzz/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num = 3;
+        var result = FizzBuzz(num);
+
+        foreach (var item in result)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    public static IList<string> FizzBuzz(int n)
+    {
+        IList<string> list = new List<string>();
+
+        for (int i = 1; i <= n; i++)
+        {
+            if(i % 3 == 0 & i % 5 == 0)
+            {
+                list.Add("FizzBuzz");
+            }
+            else if(i % 3 == 0)
+            {
+                list.Add("Fizz");
+            }
+            else if( i % 5 == 0)
+            {
+                list.Add("Buzz");
+            }
+            else
+            {
+                list.Add(i.ToString());
+            }
+        }
+        return list;
+    }
+}
