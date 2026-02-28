6719cdee64bcbeb0975e3a26d0129aa4e2cbae62
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-02-28 15:37:36 +0600

    414. Third Maximum Number C#

---
+++ CSharp/LeetCode/ThirdMax/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = [3, 2, 1];
+
+        Console.WriteLine(ThirdMax(nums));
+    }
+
+    public static int ThirdMax(int[] nums)
+    {
+        HashSet<int> set = new HashSet<int>(nums);
+        int[] newNums = set.ToArray();
+
+        Array.Sort(newNums);
+        Array.Reverse(newNums);
+
+        return newNums.Length >= 3 ? newNums[2] : newNums[newNums.Length - 1];
+    }
+}
