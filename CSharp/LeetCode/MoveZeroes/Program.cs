4a7b6001d24574fde74daaff5e8dbc9a23680975
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-03-01 16:41:21 +0600

    283. Move Zeroes C#

---
+++ CSharp/LeetCode/MoveZeroes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = [0, 1, 0, 3, 12];
+        MoveZeroes(nums);
+        //Console.WriteLine(MoveZeroes(nums));
+    }
+
+    public static void MoveZeroes(int[] nums)
+    {
+        int index = 0;
+        foreach (int x in nums)
+        {
+            if(x != 0)
+            {
+                nums[index] = x;
+                index++;
+            }
+        }
+
+        while (index < nums.Length)
+        {
+            nums[index] = 0;
+            index++;
+        }
+        
+        foreach(int x in nums)
+            Console.Write($"{x} ");
+    }
+}
