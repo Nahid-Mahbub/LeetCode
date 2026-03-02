48311191229f15cb67de2d3dacc2ab855be46e84
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-03-02 14:02:28 +0600

    1748. Sum of Unique Elements C#

---
+++ CSharp/LeetCode/SumOfUnique/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = [1, 2, 3, 2];
+        Console.WriteLine(SumOfUnique(nums));
+    }
+
+    public static int SumOfUnique(int[] nums)
+    {
+        HashSet<int> setNums = new HashSet<int>(nums);
+        
+        if(setNums.Count == nums.Length)
+        {
+            return nums.Sum(x => x);
+        }
+
+        nums.Sort();
+        HashSet<int> array = new HashSet<int>();
+        int temp = nums[0];
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (temp == nums[i])
+            {
+                array.Add(nums[i]);
+            }
+            else
+            {
+                temp = nums[i];
+            }
+        }
+
+        foreach (int i in array)
+        {
+            setNums.Remove(i);
+        }
+
+        return setNums.Sum();
+
+    }
+}
