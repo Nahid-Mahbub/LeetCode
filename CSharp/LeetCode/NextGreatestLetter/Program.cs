c759bc00eeccb6e1e90f163468b7dff4933ff5ca
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-03-03 15:14:44 +0600

    744. Find Smallest Letter Greater Than Target C#

---
+++ CSharp/LeetCode/NextGreatestLetter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        char[] letters = ['c','f','j'];
+        char target = 'c';
+        Console.WriteLine(NextGreatestLetter(letters, target));
+    }
+
+    public static char NextGreatestLetter(char[] letters, char target)
+    {
+        foreach (char c in letters)
+        {    
+            if (target < c)
+                return c;
+        }
+        return letters[0];
+    }
+}
