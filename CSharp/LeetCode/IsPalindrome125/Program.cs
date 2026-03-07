edd3c1880e5fe4dac9fbea7b759b433fbded6140
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-03-07 15:26:30 +0600

    125. Valid Palindrome C#

---
+++ CSharp/LeetCode/IsPalindrome125/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string s = "0P";
+        Console.WriteLine(IsPalindrome(s));
+    }
+
+    public static bool IsPalindrome(string s)
+    {
+        StringBuilder newString = new StringBuilder();
+        foreach (char c in s)
+        {
+            if (char.IsLetterOrDigit(c))
+            {
+                newString.Append(char.ToLower(c));
+            }
+            else
+                continue;
+        }
+
+        int left = 0;
+        int right = newString.Length - 1;
+        while (left < right)
+        {
+            if(newString[left] != newString[right])
+                return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
