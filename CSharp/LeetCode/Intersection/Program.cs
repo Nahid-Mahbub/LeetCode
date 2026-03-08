6945636a87ec546f6ecfb27c17a6ee3471a7626e
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-03-08 23:14:11 +0600

    349. Intersection of Two Arrays C#

---
+++ CSharp/LeetCode/Intersection/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums1 = [4, 9, 5];
+        int[] nums2 = [9, 4, 9, 8, 4];
+        //Console.WriteLine(Intersection(nums1, nums2));
+        foreach (int x in Intersection(nums1, nums2))
+        {
+            Console.WriteLine(x);
+        }
+    }
+
+    public static int[] Intersection(int[] nums1, int[] nums2)
+    {
+        return nums1.Intersect(nums2).ToArray();
+    }
+}
