42632d88228bc77b7b51bceaece3ae8c5979adac
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-03-11 13:28:15 +0600

    3467. Transform Array by Parity C#

---
+++ CSharp/LeetCode/TransformArray/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = [4, 3, 2, 1];
+        //Console.WriteLine(Intersection(nums1, nums2));
+        foreach (int x in TransformArray(nums))
+        {
+            Console.WriteLine(x);
+        }
+    }
+
+    public static int[] TransformArray(int[] nums)
+    {
+        List<int> answer = new List<int>(nums);
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (answer[i] % 2 == 0)
+            {
+                answer[i] = 0;
+            }
+            else
+            {
+                answer[i] = 1;
+            }
+        }
+        answer.Sort();
+        return answer.ToArray();
+    }
+}
