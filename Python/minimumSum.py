ba0722aab410c5e38bb067cca6fe0440cb7e5730
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-03-24 20:31:00 +0600

    2160. Minimum Sum of Four Digit Number After Splitting Digits

--- Python/minimumSum.py
+++ Python/minimumSum.py
@@ -1,6 +1,16 @@
 class Solution:
     def minimumSum(self, num: int) -> int:
-        
+        numList = []
+        strNums = str(num)
+        for digit in strNums:
+            numList.append(int(digit))
+        numList.sort()
+
+        # Oneline Sorted [ digits = sorted([int(d) for d in str(num)]) ]
+        new1 = numList[0] * 10 + numList[2]
+        new2 = numList[1] * 10 + numList[3]
+
+        return new1 + new2
 solution = Solution()
 num = 2932
 result = solution.minimumSum(num)
