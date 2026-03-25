376c4a4b9ea498eb98100031ed7d0b029e3dbed0
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-03-25 09:14:23 +0600

    989. Add to Array-Form of Integer C#

---
+++ CSharp/LeetCode/AddToArrayForm/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+class Program
+{
+    static void Main()
+    {
+        int[] num = [1, 2, 0, 0];
+        int k = 34;
+        var result = AddToArrayForm(num, k);
+        foreach (int i in result)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    public static IList<int> AddToArrayForm(int[] num, int k)
+    {
+        List<int> result = new List<int>();
+        int i = num.Length - 1;
+
+        while (i >= 0 || k > 0)
+        {
+            if (i >= 0)
+            {
+                k += num[i];
+                i--;
+            }
+            result.Insert(0, k % 10);
+            k /= 10;
+        }
+        return result;
+    }
+}
