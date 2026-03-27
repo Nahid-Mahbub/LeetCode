e19b286b7b0fb3ed159fa1873d48b4478523922d
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-03-27 11:43:33 +0600

    1. Two Sum C#

---
+++ CSharp/LeetCode/TwoSum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = [2, 7, 11, 15];
+        int target = 9;
+        //Console.WriteLine(Intersection(nums1, nums2));
+        foreach (int x in TwoSum(nums, target))
+        {
+            Console.WriteLine(x);
+        }
+    }
+
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return new int[] { i, j};
+                }
+            }
+        }
+        return null;
+    }
+}
