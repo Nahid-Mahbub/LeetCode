a0eb08a3b96d4c5532a0158bd6b8760c7a831b66
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-03-29 12:42:12 +0600

    1979. Find Greatest Common Divisor of Array C#

---
+++ CSharp/LeetCode/findGCD/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = [2, 5, 6, 9, 10];
+        Console.WriteLine(FindGCD(nums));
+    }
+
+    public static int FindGCD(int[] nums)
+    {
+        int numsMin = nums.Min();
+        int numsMax = nums.Max();
+
+        while (numsMin != 0)
+        {
+            int temp = numsMin;
+            numsMin = numsMax % numsMin;
+            numsMax = temp;
+        }
+        return numsMax;
+    }
+}
