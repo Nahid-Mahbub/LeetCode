d556896b8aad61d4cd1a1bced614ea1e16711e5c
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-03-31 10:15:08 +0600

    1518. Water Bottles C#

---
+++ CSharp/LeetCode/NumWaterBottles/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+
+class Program
+{
+    static void Main()
+    {
+        int numBottles = 65;
+        int numExchange = 3;
+        Console.WriteLine(NumWaterBottles(numBottles, numExchange));
+    }
+
+    public static int NumWaterBottles(int numBottles, int numExchange)
+    {
+        int result = numBottles;
+        int remain = 0;
+        while (numBottles >= numExchange)
+        {
+            result += numBottles / numExchange;
+            remain = numBottles % numExchange;
+
+            numBottles = (numBottles / numExchange) + remain;
+        }
+        return result;
+    }
+}
