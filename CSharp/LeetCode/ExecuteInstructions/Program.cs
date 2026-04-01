d5c6907dc6862efe6fd3dc440777764d824f8ad2
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-04-01 09:42:03 +0600

    2120. Execution of All Suffix Instructions Staying in a Grid init c#

---
+++ CSharp/LeetCode/ExecuteInstructions/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 3;
+        int[] startPos = [0, 1];
+        string s = "RRDDLU";
+         foreach (int i in ExecuteInstructions(n, startPos, s))
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    public static int[] ExecuteInstructions(int n, int[] startPos, string s)
+    {
+        return new int[] { 0, 1, 2 };
+    }
+}
