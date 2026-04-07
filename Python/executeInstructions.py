c486539c6f9372292a32184a45f6b64f1cef1094
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-04-07 07:59:35 +0600

    2120. Execution of All Suffix Instructions Staying in a Grid py

--- Python/executeInstructions.py
+++ Python/executeInstructions.py
@@ -1,23 +1,29 @@
 class Solution:
     def executeInstructions(self, n: int, startPos: list[int], s: str) -> list[int]:
-        counter = 0
-        for char in s:
-            print(char)
-            if (char == 'R' and startPos[1] < n-1):
-                startPos[1] = startPos[1] + 1
+        
+        result = []
+        for i in range(len(s)):
+            x, y = startPos
+            counter = 0
+
+            for j in range(i, len(s)):
+                if s[j] == 'R':
+                    y += 1
+                elif s[j] == 'L':
+                    y -= 1
+                elif s[j] == 'U':
+                    x -= 1
+                elif s[j] == 'D':
+                    x += 1
+                
+                if x < 0 or x >= n or y < 0 or y >= n:
+                    break
+                
                 counter += 1
-            elif (char == 'L' and startPos[1] > 0):
-                startPos[1] = startPos[1] - 1
-                counter += 1
-            elif (char == 'U' and startPos[0] > 0):
-                startPos[0] = startPos[0] - 1
-                counter += 1
-            elif (char == 'D' and startPos[0] < n-1):
-                startPos[0] = startPos[0] + 1
-                counter += 1
-            else:
-                return counter
-        return counter
+            
+            result.append(counter)
+            
+        return result
 
 solution = Solution()
 n = 3
