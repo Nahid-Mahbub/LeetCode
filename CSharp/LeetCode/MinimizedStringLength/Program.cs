1d96aed71c8b3fbf48dca724c2b03a7ba253c97f
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-04-09 10:28:26 +0600

    2716. Minimize String Length C#

---
+++ CSharp/LeetCode/MinimizedStringLength/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics.Metrics;
+
+class Program
+{
+    static void Main()
+    {
+        string s = "aaabc";
+        Console.WriteLine(MinimizedStringLength(s));
+    }
+
+    public static int MinimizedStringLength(string s)
+    {
+        HashSet<char> chr = new HashSet<char>(s);
+        //foreach (char c in s)
+        //{
+        //    chr.Add(c);
+        //}
+        return chr.Count();
+    }
+}
