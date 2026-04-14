9442f1105f5e4af55c4dca3556f6abae9d81892b
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-04-14 11:58:50 +0600

    977. Squares of a Sorted Array C#

---
+++ CSharp/LeetCode/SortedSquares/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics.Metrics;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = [-4, -1, 0, 3, 10];
+        foreach(int i in SortedSquares(nums))
+        {
+            Console.WriteLine(i);
+        }
+
+    }
+
+    public static int[] SortedSquares(int[] nums)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            nums[i]= nums[i] * nums[i];
+        }
+        nums.Sort();
+
+        return nums;
+    }
+}
