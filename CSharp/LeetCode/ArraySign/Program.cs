c8c996fe6ae0cd1fa1bf7edbfb1ac8c4fc210868
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-04-18 07:58:00 +0600

    1822. Sign of the Product of an Array C#

---
+++ CSharp/LeetCode/ArraySign/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics.Metrics;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = { 9, 72, 34, 29, -49, -22, -77, -17, -66, -75, -44, -30, -24 };
+        Console.WriteLine(ArraySign(nums));
+    }
+
+    public static int ArraySign(int[] nums)
+    {
+        if (nums.Contains(0))
+            return 0;
+
+        int productSign = 1;
+        foreach (int num in nums)
+        {
+            if (num > 0)
+                productSign *= 1;
+            else
+                productSign *= -1;
+        }
+        return productSign;
+    }
+}
