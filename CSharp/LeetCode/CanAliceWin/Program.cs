49363513452c7d2eb90774aca7c70863a27a810d
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-04-19 10:10:01 +0600

    3232. Find if Digit Game Can Be Won C#

---
+++ CSharp/LeetCode/CanAliceWin/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics.Metrics;
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = {1, 2, 3, 4, 10};
+        Console.WriteLine(CanAliceWin(nums));
+    }
+
+    public static bool CanAliceWin(int[] nums)
+    {
+        int singleNum = 0;
+        int doubleNum = 0;
+        foreach (int i in nums)
+        {
+            if (i < 10)
+            {
+                singleNum += i;
+            }
+            else
+            {
+                doubleNum += i;
+            }
+        }
+        return singleNum == doubleNum ? false : true;
+    }
+}
