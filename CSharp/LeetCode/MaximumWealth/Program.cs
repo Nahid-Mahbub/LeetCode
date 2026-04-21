e5cdb17e9df47bac2631e5793043b24b8d60eeb5
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-04-21 10:35:28 +0600

    1672. Richest Customer Wealth C#

---
+++ CSharp/LeetCode/MaximumWealth/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics.Metrics;
+
+class Program
+{
+    static void Main()
+    {
+        int[][] accounts = [[2, 8, 7], [7, 1, 3], [1, 9, 5]];
+        Console.WriteLine(MaximumWealth(accounts));
+    }
+
+    public static int MaximumWealth(int[][] accounts)
+    {
+        int maxWealth = 0;
+        foreach (int[] account in accounts)
+        {
+            if(maxWealth < account.Sum()) {
+                maxWealth = account.Sum();
+            }
+        }
+        return maxWealth;
+    }
+}
