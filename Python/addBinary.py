517db80233c8a71d6d0c9407d03e762a111d58f0
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-04-23 18:23:02 +0600

    67. Add Binary py

---
+++ Python/addBinary.py
@@ -0,0 +1,11 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        num = int(a, 2) + int(b, 2)
+
+
+        return str(bin(num)[2:])
+solution = Solution()
+a = "11"
+b = "1"
+result = solution.addBinary(a, b)
+print(result)
