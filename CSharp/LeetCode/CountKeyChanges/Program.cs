4a8e5d70090727000c0a2af79fd00389d8ff0eb2
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-05-03 13:11:19 +0600

    3019. Number of Changing Keys C#

---
+++ CSharp/LeetCode/CountKeyChanges/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics.Metrics;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static void Main()
+    {
+        string s = "aAbBcC";
+        Console.WriteLine(CountKeyChanges(s));
+    }
+
+    public static int CountKeyChanges(string s)
+    {
+        s= s.ToLower();
+        int count = 0;
+
+        for (int i = 1; i < s.Length; i++) {
+            if (s[i] != s[i - 1]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
