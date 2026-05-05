d97ab5b0435e4d888ec271c3cc14e21c9b64cd54
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-05-05 19:25:15 +0600

    849. Maximize Distance to Closest Person py

--- Python/maxDistToClosest.py
+++ Python/maxDistToClosest.py
@@ -1,8 +1,27 @@
 class Solution:
     def maxDistToClosest(self, seats: list[int]) -> int:
-        int current = 0
+        left = 0
+        mid = 0
+        right = 0
+        temp = 0
+        first_one_found = False
+
         for seat in seats:
+            if(seat == 0):
+                temp += 1
             
+            else:
+                if not first_one_found:
+                    left = temp
+                    first_one_found = True
+                else:
+                    mid = max(mid, (temp + 1) // 2)
+
+                temp = 0
+        right = temp
+
+        return max(right, left, mid)
+
 solution = Solution()
 seats = [1,0,0,0,1,0,1]
 result = solution.maxDistToClosest(seats)
