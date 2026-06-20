0e9854dc660f807904307b3f0b49554c9dd1f87c
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-06-20 18:31:28 +0600

    1861. Rotating the Box py

--- Python/rotateTheBox.py
+++ Python/rotateTheBox.py
@@ -3,19 +3,23 @@ class Solution:
         
         print(boxGrid)
         print()
-        for i in range(len(boxGrid)):
-            j = len(boxGrid[i]) - 1
-            for j in range(len(boxGrid[i])-1, -1, -1):
-                if (boxGrid[i][j] == '.'):
-                    if (boxGrid[i][j-1] == "*" or j == 0):
-                        continue
-                    
-                    temp = boxGrid[i][j]
-                    boxGrid[i][j] = boxGrid[i][j-1]
-                    boxGrid[i][j-1] = temp
+        boxGrid_len = len(boxGrid)
+        boxGrid0_len = len(boxGrid[0])
+        for i in range(boxGrid_len):
+            gravity = boxGrid0_len - 1
+            for j in range(boxGrid0_len-1, -1, -1):
+                if (boxGrid[i][j] == '*'):
+                    gravity = j - 1
+                elif (boxGrid[i][j] == '#'):
+                    boxGrid[i][j] = '.'
+                    boxGrid[i][gravity] = '#'
+                    gravity -= 1
                     print(boxGrid)
+        
+        print()
         print(boxGrid)
         print()
+
         result = []
         for i in range(len(boxGrid[0])):
             temp = []
