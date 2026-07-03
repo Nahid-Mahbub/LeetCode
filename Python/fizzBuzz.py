80319404c43ebaceb7e8db21a9f073236f54afe7
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-07-03 14:29:59 +0600

    Fixing commit problem with pc

--- Python/fizzBuzz.py
+++ Python/fizzBuzz.py
@@ -1,18 +1,55 @@
-class Solution:
-    def fizzBuzz(self, n: int) -> list[str]:
-        output = []
-        for i in range(1, n+1):
-            if(i % 3 == 0 and i % 5 == 0):
-                output.append("FizzBuzz")
-            elif(i % 3 == 0):
-                output.append("Fizz")
-            elif(i % 5 == 0):
-                output.append("Buzz")
+from threading import Semaphore
+from typing import Callable
+
+class FizzBuzz:
+
+    def __init__(self, n: int):
+        self.n = n
+        
+        self.fizz_sem = Semaphore(0)
+        self.buzz_sem = Semaphore(0)
+        self.fizzbuzz_sem = Semaphore(0)
+        self.number_sem = Semaphore(1)
+
+
+    def fizz(self, printFizz: Callable[[], None]) -> None:
+        for i in range(1, self.n + 1):
+            if i % 3 == 0 and i % 5 != 0:
+                self.fizz_sem.acquire()
+                printFizz()
+                self.number_sem.release()
+
+
+    def buzz(self, printBuzz: Callable[[], None]) -> None:
+        for i in range(1, self.n + 1):
+            if i % 5 == 0 and i % 3 != 0:
+                self.buzz_sem.acquire()
+                printBuzz()
+                self.number_sem.release()
+
+
+    def fizzbuzz(self, printFizzBuzz: Callable[[], None]) -> None:
+        for i in range(1, self.n + 1):
+            if i % 15 == 0:
+                self.fizzbuzz_sem.acquire()
+                printFizzBuzz()
+                self.number_sem.release()
+
+
+    def number(self, printNumber: Callable[[int], None]) -> None:
+        for i in range(1, self.n + 1):
+
+            self.number_sem.acquire()
+
+            if i % 15 == 0:
+                self.fizzbuzz_sem.release()
+
+            elif i % 3 == 0:
+                self.fizz_sem.release()
+
+            elif i % 5 == 0:
+                self.buzz_sem.release()
+
             else:
-                output.append(str(i))
-        return output
-            
-solution = Solution()
-n = 3
-result = solution.fizzBuzz(n)
-print(result)
+                printNumber(i)
+                self.number_sem.release()
