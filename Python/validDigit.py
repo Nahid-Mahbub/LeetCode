a85f04fe24927c5ff3a51d43021062176e199437
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-07-21 14:22:02 +0600

    3908. Valid Digit Number py

---
+++ Python/validDigit.py
@@ -0,0 +1,13 @@
+class Solution:
+    def validDigit(self, n: int, x: int) -> bool:
+        str_n = str(n)
+        str_x = str(x)
+        for i in range(len(str_n)):
+            if str_n[i] == str_x and i != 0:
+                return True
+        return False
+solution = Solution()
+n = 202
+x = 2
+result = solution.validDigit(n, x)
+print(result)
