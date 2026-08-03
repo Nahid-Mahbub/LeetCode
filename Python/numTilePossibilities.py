d1da96c5c9954a4d87114e0db8c6a73a6aa8bb8f
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-08-03 23:56:48 +0600

    1079. Letter Tile Possibilities py

---
+++ Python/numTilePossibilities.py
@@ -0,0 +1,7 @@
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
+        
+solution = Solution()
+tiles = "AAABBC"
+result = solution.numTilePossibilities(tiles)
+print(result)
