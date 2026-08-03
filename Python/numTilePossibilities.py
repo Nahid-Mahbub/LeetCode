62a490eed70942bf13f036df3fd68ef73cade6b3
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-08-04 00:08:19 +0600

    1079. Letter Tile Possibilities py

--- Python/numTilePossibilities.py
+++ Python/numTilePossibilities.py
@@ -1,7 +1,21 @@
+from collections import Counter
 class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
         
+        def dfs(counter):
+
+            ans = 0
+            for ch in counter:
+                if counter[ch] == 0:
+                    continue
+                ans += 1
+                counter[ch] -= 1
+                ans += dfs(counter)
+                counter[ch] += 1
+            return ans
+        return dfs(Counter(tiles))
+
 solution = Solution()
-tiles = "AAABBC"
+tiles = "AAB"
 result = solution.numTilePossibilities(tiles)
 print(result)
