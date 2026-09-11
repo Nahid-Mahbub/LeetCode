7cb39662e396ec9afda344ae1b08551fdbd971c9
Nahid Mahbub <mahbub2305101632@diu.edu.bd>
2026-09-11 09:51:44 +0600

    1791. Find Center of Star Graph py

--- Python/findCenter.py
+++ Python/findCenter.py
@@ -1,3 +1,15 @@
 class Solution:
-    def findCenter(self, edges: List[List[int]]) -> int:
-        
+    def findCenter(self, edges: list[list[int]]) -> int:
+
+        dic_edges = {}
+        for i in range(len(edges)):
+            dic_edges[edges[i][0]] = dic_edges.get(edges[i][0], 0) + 1
+            dic_edges[edges[i][1]] = dic_edges.get(edges[i][1], 0) + 1
+        print(dic_edges)
+       
+        return max(dic_edges, key=dic_edges.get)
+    
+solution = Solution()
+edges = [[1,2],[5,1],[1,3],[1,4]]
+result = solution.findCenter(edges)
+print(result)
